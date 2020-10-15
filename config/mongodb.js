fbc9dca13b7d02c4a40410a906238aaccb7e7ce3
Muram M <Ma2moiselleM@MacBook-Pro-de-Muram.local>
2020-10-15 10:33:47 +0200

    changed config with try catch

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on("error", () => {
-  console.error("NAY : database connection failed !!!");
-});
-
-mongoose.connection.on("connected", () => {
-  console.log("YAY : Database connected !!!",'@', db);
-  console.log("clik here to acces the website =>", `http://localhost:${process.env.PORT}`);
+try {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   
-});
+  mongoose.connection.on("error", () => {
+    console.error("NAY : database connection failed !!!");
+  });
+  
+  mongoose.connection.on("connected", () => {
+    console.log("YAY : Database connected !!!",'@', db);
+    console.log("clik here to acces the website =>", `http://localhost:${process.env.PORT}`);
+    
+  });
+} catch (error) {
+  console.log(error);
+  
+}

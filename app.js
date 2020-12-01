b546163a1edb31e5baf94d9254a2bce10cc6ef82
Muram M <Ma2moiselleM@MacBook-Pro-de-Muram.local>
2020-12-01 11:14:45 +0100

    edited some pages to scale footer

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 require("./helpers/hbs"); // utils for hbs templates
 require("./config/mongodb"); // database initial setup
-var createError = require('http-errors');
-var express = require('express');
-server = express()
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+server = express();
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 const hbs = require("hbs");
 const flash = require("connect-flash");
-const dev_mode = false;
+const dev_mode = true;
 
 var app = express();
 
@@ -32,7 +32,7 @@ app.use(
 app.use(flash());
 
 // app.use(function(req, res, next){
-//   // if there's a flash message in the session request, make it available 
+//   // if there's a flash message in the session request, make it available
 //   //in the response, then delete it
 //     res.locals.sessionFlash = req.session.sessionFlash;
 //     delete req.session.sessionFlash;
@@ -53,46 +53,41 @@ app.use(require("./middlewares/exposeFlashMessage")); // affiche les messages da
 // view engine setup
 // app.set('views', path.join(__dirname, '/views'));
 app.set("views", __dirname + "/views");
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 app.use(cors("*")); //toujours avant les use des routes
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 hbs.registerPartials(__dirname + "/views/partials");
-
 
 // routers use
 
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
-app.use('/userboard', require('./routes/userboard'));
-app.use('/dashboard', require('./routes/dashboard'));
-app.use('/ateliers', require('./routes/ateliers'));
-app.use('/categories', require('./routes/categories'));
-app.use('/articles', require('./routes/articles'));
-app.use('/auth', require('./routes/auth'));
-
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+app.use("/userboard", require("./routes/userboard"));
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/ateliers", require("./routes/ateliers"));
+app.use("/categories", require("./routes/categories"));
+app.use("/articles", require("./routes/articles"));
+app.use("/auth", require("./routes/auth"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-
-
 module.exports = app;
- 
